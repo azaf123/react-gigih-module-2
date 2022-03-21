f27a3e0dad4a892defe95e56e25b14111df71861
azaf123 <ridhofaza1234@gmail.com>
2022-03-21 23:46:59 +0700

    make component of  image of album

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import { Component, Fragment } from 'react';
 import './App.css';
 
+class AlbumImage extends Component{
+  render(){
+    return(
+      <Fragment>
+        <img src="https://picsum.photos/400/300" className="image-song" alt=""></img>
+      </Fragment>
+    )
+  }
+}
+
 function App() {
-  console.log(process.env.REACT_APP_GIPHY_KEY);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='row'>
+          <AlbumImage />
+      </div>
     </div>
   );
 }
