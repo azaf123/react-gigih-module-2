a19f663e1b40bad85d78f62fade6542d047f9b72
azaf123 <ridhofaza1234@gmail.com>
2022-04-22 22:47:29 +0700

    redirect_uri changed

--- src/components/navbar/login/index.jsx
+++ src/components/navbar/login/index.jsx
@@ -10,7 +10,7 @@ import { setAccessToken } from '../../../redux/slices/tokenSlice';
 
 const AUTH_URL = 'https://accounts.spotify.com/authorize';
 const client_id = `${process.env.REACT_APP_SPOTIFY_CLIENT_ID}`;
-const redirect_uri = 'https://react-gigih.vercel.app/';
+const redirect_uri = 'https://react-gigih-module-2-test.vercel.app/';
 const scopes = 'user-read-private user-read-email playlist-modify-private playlist-read-private playlist-modify-public playlist-read-collaborative user-read-playback-state user-modify-playback-state user-read-currently-playing user-read-recently-played user-top-read user-follow-read user-follow-modify user-library-read user-library-modify';
 const url = `${AUTH_URL}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scopes}&response_type=token&show_dialog=true`;
 
